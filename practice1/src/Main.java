672eaedc5043d2e6edbaf1e19c59b1c0e7642f8a
guoliangchen200171 <chen.guol@northeastern.edu>
2025-10-28 02:47:04 -0700

    make a new branch

--- practice1/src/Main.java
+++ practice1/src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("1112312312");
         System.out.println("good year");
         System.out.println("bridgestone");
-        System.out.println("21321213123123");
+        System.out.println("299999999999987821");
     }
 }
